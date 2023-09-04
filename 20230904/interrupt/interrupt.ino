0d71c0c25e05d6c38204ea49bcbcc9609f643165
kimtaemin1324 <125728601+kimtaemin1324@users.noreply.github.com>
2023-09-04 11:36:36 +0900

    Add files via upload

---
+++ 20230904/interrupt/interrupt.ino
@@ -0,0 +1,66 @@
+const byte outPin = 13; // Output pin: digital pin 13(D13)
+const byte interruptPin1 = 2; // Interrupt pin: D2
+const byte interruptPin2 = 3; // Interrupt pin: D2
+const byte resetPin = 5;
+volatile byte state = 0;
+unsigned long cnt1 = 0;
+unsigned long cnt2 = 0;
+
+
+void setup()
+{
+  pinMode(outPin, OUTPUT); // Output mode
+  pinMode(interruptPin1, INPUT_PULLUP); // Input mode, pull-up
+  pinMode(interruptPin2, INPUT_PULLUP); // Input mode, pull-up
+  pinMode(resetPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(interruptPin1), intfunc1, RISING); // Enable interrupt
+  attachInterrupt(digitalPinToInterrupt(interruptPin2), intfunc2, RISING); // Enable interrupt
+  Serial.begin(115200);
+}
+void intfunc1() // Interrupt function
+{
+  cnt1++;
+  if (state == 0) // If D4 output is low
+  {
+    digitalWrite(outPin, HIGH);
+    state = 1;
+  }
+  else
+  {
+    digitalWrite(outPin, LOW);
+    state = 0;
+  }
+}
+void intfunc2() // Interrupt function
+{
+  cnt2++;
+  if (state == 0) // If D4 output is low
+  {
+    digitalWrite(outPin, HIGH);
+    state = 1;
+  }
+  else
+  {
+    digitalWrite(outPin, LOW);
+    state = 0;
+  }
+}
+
+
+void loop()
+{
+  Serial.print("cnt1: ");
+  Serial.print(cnt1);
+  if (digitalRead (resetPin) == HIGH)
+  {
+    cnt1 = 0;
+  }
+
+  Serial.print("   cnt2: ");
+  Serial.println(cnt2);
+  if (digitalRead (resetPin) == HIGH)
+  {
+    cnt2 = 0;
+  }
+  delay(1000);
+}
